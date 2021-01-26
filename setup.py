b45085d8ef02c55a9abce20864bb64eb88365deb
Brian McClune <bpmcclune@gmail.com>
2021-01-26 15:24:52 -0500

    Create dochunt-api server based on fastapi docs

---
+++ setup.py
@@ -0,0 +1,23 @@
+"""Dochunt server setup script.
+
+"""
+
+import setuptools
+
+
+setuptools.setup(
+    name='dochunt-api',
+    version='0.0.1',
+    author='Brian McClune',
+    author_email='author@example.com',
+    description='An API for finding documents by likeness',
+    url='https://github.com/McClunatic/dochunt-vue3',
+    package_dir={'': 'src'},
+    packages=setuptools.find_packages(where='src'),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.8',
+)
