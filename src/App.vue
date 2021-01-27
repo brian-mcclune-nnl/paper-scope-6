f966ac573bd440dde1b8b65570048ea46f423e0f
Brian McClune <bpmcclune@gmail.com>
2021-01-27 07:38:11 -0500

    Add API call to dochunt-api vs axios

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <HelloWorld :msg="msg" />
 </template>
 
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import { onMounted, ref } from 'vue';
+
+const msg = ref("Hello Vue 3 + Vite");
+
+const getMsg = () => {
+  axios.get('http://127.0.0.1:21383')
+    .then(function (res) {
+      msg.value = res.data.message;
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+}
+
+onMounted(getMsg);
 </script>
 
 <style>
