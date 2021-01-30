47122e747c816c24425ab8a13aa37af1286591d9
Brian McClune <bpmcclune@gmail.com>
2021-01-30 16:38:07 -0500

    Update msal and app to enable clunacy signin

--- client/src/store/modules/msal.js
+++ client/src/store/modules/msal.js
@@ -4,25 +4,39 @@ const state = () => ({
     auth: {
       clientId: '7a0d396a-c9aa-4a65-8bff-e906fef7b0c7',
       authority:
-      'https://login.microsoftonline.com/07f0848e-60d0-4c23-8482-cebd7ab04d72',
+      'https://clunacy.b2clogin.com/clunacy.onmicrosoft.com/' +
+      'B2C_1_dochuntsignupsignin',
+      knownAuthories: 'clunacy.b2clogin.com',
+      redirectUri: 'https://localhost:3000/'
     },
     cache: {
       cacheLocation: 'localStorage',
     },
   },
-  accessToken: ''
+  accessToken: '',
+  account: null
 })
 
 // getters
 const getters = {}
 
-// actions
-const actions = {}
-
 // mutations
 const mutations = {
   setAccessToken(state, token){
     state.accessToken = token;
+  },
+  setAccount(state, account){
+    state.account = account;
+  }
+}
+
+// actions
+const actions = {
+  setAccessToken(context, token){
+    context.commit('setAccessToken', token);
+  },
+  setAccount(context, account){
+    context.commit('setAccount', account);
   }
 }
 
