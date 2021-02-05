bccd0786860b18687e236bd3e73a4c3fdb92146a
Brian McClune <bpmcclune@gmail.com>
2021-02-05 17:27:14 -0500

    Update authority for new user flow

--- src/store/modules/msal.js
+++ src/store/modules/msal.js
@@ -5,7 +5,7 @@ const state = () => ({
       clientId: 'f95c6431-ef71-4809-ab02-0c94c11a71ee',
       authority:
       'https://clunacy.b2clogin.com/clunacy.onmicrosoft.com/' +
-      'B2C_1_dochuntsignupsignin',
+      'B2C_1_dochunt_signupsignin',
       knownAuthories: 'clunacy.b2clogin.com',
       redirectUri: import.meta.env.PROD
         ? 'https://dochunt-vue3-spa.azurewebsites.net/'
