3a82b7a0f950ce887869751f8cc483d8beb3434f
Brian McClune <bpmcclune@gmail.com>
2021-02-18 16:59:29 -0500

    Move global msalInstance to msal store module

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,16 @@
 import { computed } from "vue"
 import { createWebHistory, createRouter } from "vue-router"
-import { PublicClientApplication } from "@azure/msal-browser";
 import store from "../store"
 import Home from "../views/Home.vue"
 import Unauthenticated from "../views/Unauthenticated.vue"
 
 const routes = [
   {
-    path: '/',
+    path: "/",
     component: Home
   },
   {
-    path: '/unauthenticated',
+    path: "/unauthenticated",
     component: Unauthenticated
   }
 ]
@@ -21,23 +20,15 @@ const router = createRouter({
   routes
 })
 
-const msalConfig = computed(() => store.state.msal.config);
 const account = computed(() => store.state.msal.account);
 
-// The msalInstance gets mutated when loginPopup() is called
-// which means if made an attribute of the Vuex store state,
-// it causes errors even when called in a mutation.
-// The workaround is to have a global instance somewhere else
-// in the application.
-const msalInstance = new PublicClientApplication(msalConfig.value);
-
 router.beforeEach(async (to, from) => {
-  if (to.path !== '/unauthenticated' && account.value === null) {
+  if (to.path !== "/unauthenticated" && account.value === null) {
     try {
-      await store.dispatch('msal/login', msalInstance)
+      await store.dispatch("msal/login")
     } catch (error) {
       console.log(error)
-      return '/unauthenticated'
+      return "/unauthenticated"
     }
   }
 })
--- src/store/modules/msal.js
+++ src/store/modules/msal.js
@@ -1,7 +1,15 @@
 import {
+  PublicClientApplication,
   BrowserAuthError,
   InteractionRequiredAuthError
 } from "@azure/msal-browser";
+
+// The msalInstance gets mutated when loginPopup() is called
+// which means if made an attribute of the Vuex store state,
+// it causes errors even when called in a mutation.
+// The workaround is to have a global instance somewhere else
+// in the application.
+var msalInstance = null;
 
 // initial state
 const state = () => ({
@@ -24,7 +32,8 @@ const state = () => ({
     'https://clunacy.onmicrosoft.com/' +
     '7ea70574-6eb1-42f2-ab65-d88d33884ccb/user.impersonate',
   ],
-  account: null
+  account: null,
+  instanceCreated: false
 })
 
 // getters
@@ -32,32 +41,39 @@ const getters = {}
 
 // mutations
 const mutations = {
-  login(state, { account, instance }) {
+  createInstance(state) {
+    if (!state.instanceCreated) {
+      msalInstance = new PublicClientApplication(state.config);
+      state.instanceCreated = true;
+    }
+  },
+  login(state, account) {
     state.account = account;
-    instance.setActiveAccount(account);
+    msalInstance.setActiveAccount(account);
   }
 }
 
 // actions
 const actions = {
-  createInstance(context){
+  createInstance(context) {
     context.commit('createInstance');
   },
-  async login({ commit, state }, instance){
+  async login({ commit, state }) {
+    if (!state.instanceCreated) commit('createInstance');
     const loginRequest = {
       scopes: state.scopes,
       loginHint: state.account ? state.account.username : ""
     };
     let loginResponse = null;
     try {
-      loginResponse = await instance.ssoSilent(loginRequest)
+      loginResponse = await msalInstance.ssoSilent(loginRequest)
     } catch (error) {
       if (!error instanceof InteractionRequiredAuthError &&
           !error instanceof BrowserAuthError )
         throw error;
-      loginResponse = await instance.loginPopup(loginRequest);
+      loginResponse = await msalInstance.loginPopup(loginRequest);
     }
-    commit('login', { account: loginResponse.account, instance });
+    commit('login', loginResponse.account);
   }
 }
 
