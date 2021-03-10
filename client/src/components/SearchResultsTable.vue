2037d5629fc358b7f7ddeae2b6ace31402329f65
Brian McClune <bpmcclune@gmail.com>
2021-03-10 10:39:37 -0500

    Add multi-column sorting to search results table

--- client/src/components/SearchResultsTable.vue
+++ client/src/components/SearchResultsTable.vue
@@ -6,29 +6,43 @@
     Loading...
   </p>
   <table
-    v-else-if="displayResults.length > 0"
+    v-else-if="results.length > 0"
     class="table margined"
   >
     <thead>
       <tr>
         <th
-          v-for="(value, name) in displayResults[0]"
+          v-for=" name in columns"
           :key="name"
         >
-          {{ name }}
+          <span @click="updateSortColumns(name)">
+            {{ name }}
+            <span class="icon">
+              <i :class="['fas', sortIcon(name)]" />
+            </span>
+          </span>
+          <span
+            v-if="sortIndex(name) > -1"
+            @click="removeSortColumns(name)"
+          >
+            {{ sortIndex(name) + 1 }}
+            <span class="icon">
+              <i class="fas fa-times-circle" />
+            </span>
+          </span>
         </th>
       </tr>
     </thead>
     <tbody>
       <tr
-        v-for="result in displayResults"
+        v-for="result in sortedResults"
         :key="result.id"
       >
         <td
-          v-for="(value, name) in result"
+          v-for="name in columns"
           :key="name"
         >
-          {{ value }}
+          {{ result[name] }}
         </td>
       </tr>
     </tbody>
@@ -42,27 +56,46 @@
 </template>
 
 <script setup>
-import { computed, watch } from 'vue'
+import { computed, ref, watch } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
-
-export default {
-  props: {
-    results: {
-      type: Array,
-      required: true
-    }
-  }
-}
 
 const route = useRoute()
 const store = useStore()
 const results = computed(() => store.state.search.results)
 const loading = computed(() => store.state.search.loading)
 
-const displayResults = computed(() => {
-  const cond = (({ id, title, author, date}) => ({ id, title, author, date}))
-  return results.value.map(cond)
+const columns = ['id', 'title', 'author', 'date']
+const sortColumns = ref([])
+
+const sortIcon = name => {
+  const found = sortColumns.value.find(elem => elem.name === name)
+  if (found === undefined) return 'fa-sort'
+  else return found.descending ? 'fa-sort-down' : 'fa-sort-up'
+}
+
+const updateSortColumns = name => {
+  const found = sortColumns.value.find(elem => elem.name === name)
+  if (found === undefined) sortColumns.value.push({ name, descending: true })
+  else found.descending = !found.descending
+}
+
+const sortIndex = name => sortColumns.value.findIndex(el => el.name === name)
+
+const removeSortColumns = name => {
+  sortColumns.value.splice(sortIndex(name), 1)
+}
+
+const sortedResults = computed(() => {
+  if (sortColumns.value.length === 0) return results.value
+  else return [...results.value].sort((a, b) => {
+    for (let column of sortColumns.value) {
+      console.log(column)
+      if (a[column.name] < b[column.name]) return -column.descending
+      else if (a[column.name] > b[column.name]) return column.descending
+    }
+    return 0
+  })
 })
 
 watch(
@@ -75,5 +108,9 @@ watch(
 <style>
 .margined {
   margin-top: 16px;
+}
+
+span.icon {
+  margin-left: 1rem;
 }
 </style>
