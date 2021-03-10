1427eab2ecc883de0eb762539e875b5d6b9d6b38
Brian McClune <bpmcclune@gmail.com>
2021-03-09 20:44:51 -0500

    Refactor search results table into sep. component

---
+++ client/src/components/SearchResultsTable.vue
@@ -0,0 +1,79 @@
+<template>
+  <p
+    v-if="loading"
+    class="has-text-centered"
+  >
+    Loading...
+  </p>
+  <table
+    v-else-if="displayResults.length > 0"
+    class="table margined"
+  >
+    <thead>
+      <tr>
+        <th
+          v-for="(value, name) in displayResults[0]"
+          :key="name"
+        >
+          {{ name }}
+        </th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr
+        v-for="result in displayResults"
+        :key="result.id"
+      >
+        <td
+          v-for="(value, name) in result"
+          :key="name"
+        >
+          {{ value }}
+        </td>
+      </tr>
+    </tbody>
+  </table>
+  <p
+    v-else
+    class="has-text-centered"
+  >
+    No results to display.
+  </p>
+</template>
+
+<script setup>
+import { computed, watch } from 'vue'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+
+export default {
+  props: {
+    results: {
+      type: Array,
+      required: true
+    }
+  }
+}
+
+const route = useRoute()
+const store = useStore()
+const results = computed(() => store.state.search.results)
+const loading = computed(() => store.state.search.loading)
+
+const displayResults = computed(() => {
+  const cond = (({ id, title, author, date}) => ({ id, title, author, date}))
+  return results.value.map(cond)
+})
+
+watch(
+  () => route.params,
+  () => store.dispatch('search/updateResults'),
+  { immediate: true }
+)
+</script>
+
+<style>
+.margined {
+  margin-top: 16px;
+}
+</style>
--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -3,79 +3,18 @@
     <nav-bar class="is-flow-grow-0" />
     <div class="columns is-centered is-vcentered is-flex-grow-1">
       <div class="container is-fluid is-centered">
-        <p
-          v-if="loading"
-          class="has-text-centered"
-        >
-          Loading...
-        </p>
-        <table
-          v-else-if="results.length > 0"
-          class="table margined"
-        >
-          <thead>
-            <tr>
-              <th
-                v-for="(value, name) in displayResults[0]"
-                :key="name"
-              >
-                {{ name }}
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="result in displayResults"
-              :key="result.id"
-            >
-              <td
-                v-for="(value, name) in result"
-                :key="name"
-              >
-                {{ value }}
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <p
-          v-else
-          class="has-text-centered"
-        >
-          No results to display.
-        </p>
+        <search-results-table />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { computed, watch } from 'vue'
-import { useRoute } from 'vue-router'
-import { useStore } from 'vuex'
 import NavBar from '../components/NavBar.vue'
-
-const route = useRoute()
-const store = useStore()
-const results = computed(() => store.state.search.results)
-const loading = computed(() => store.state.search.loading)
-
-const displayResults = computed(() => {
-  const cond = (({ id, title, author, date}) => ({ id, title, author, date}))
-  return results.value.map(cond)
-})
-
-watch(
-  () => route.params,
-  () => store.dispatch('search/updateResults'),
-  { immediate: true }
-)
+import SearchResultsTable from '../components/SearchResultsTable.vue'
 </script>
 
 <style>
-.margined {
-  margin-top: 16px;
-}
-
 .is-flex.is-flex-direction-column {
   height: 98%;
 }
