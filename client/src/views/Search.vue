b2be62049136f31e06e42c85222176baaa3c932f
Brian McClune <bpmcclune@gmail.com>
2021-03-12 10:15:55 -0500

    Adjust search view to center results as is-11 col

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -2,7 +2,7 @@
   <div class="is-flex is-flex-direction-column">
     <nav-bar class="is-flow-grow-0" />
     <div class="columns is-centered is-vcentered is-flex-grow-1">
-      <div class="container is-fluid is-centered">
+      <div class="column is-11 is-centered">
         <search-results-table />
       </div>
     </div>
