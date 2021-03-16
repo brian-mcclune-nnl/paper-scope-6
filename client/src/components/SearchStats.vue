315d01e7ca0868a00287242c8a7edf9366b8521c
Brian McClune <bpmcclune@gmail.com>
2021-03-16 08:12:17 -0400

    Add ellipsis loading animation to SearchStats

--- client/src/components/SearchStats.vue
+++ client/src/components/SearchStats.vue
@@ -1,9 +1,9 @@
 <template>
   <div
-    v-if="!loading"
     class="has-text-justified"
+    :class="{ loading: loading }"
   >
-    {{ results.length }} results found ({{ time }} ms)
+    {{ statsMessage }}
   </div>
 </template>
 
@@ -16,10 +16,40 @@
   const loading = computed(() => store.state.search.loading)
   const results = computed(() => store.state.search.results)
   const time = computed(() => store.state.search.time)
+
+  const statsMessage = computed(() => (
+    loading.value
+      ? 'Loading'
+      : `${results.value.length} results found (${time.value} ms)`
+  ))
 </script>
 
 <style>
   .has-text-justified {
     font-size: 0.9rem;
+  }
+
+  /* https://stackoverflow.com/questions/13014808/
+     is-there-anyway-to-animate-an-ellipsis-with-css-animations */
+  .loading:after {
+    overflow: hidden;
+    display: inline-block;
+    vertical-align: bottom;
+    -webkit-animation: ellipsis steps(4,end) 900ms infinite;
+    animation: ellipsis steps(4,end) 900ms infinite;
+    content: "\2026"; /* ascii code for the ellipsis character */
+    width: 0px;
+  }
+
+  @keyframes ellipsis {
+    to {
+      width: 20px;
+    }
+  }
+
+  @-webkit-keyframes ellipsis {
+    to {
+      width: 20px;
+    }
   }
 </style>
