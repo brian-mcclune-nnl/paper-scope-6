00960c4b92ec090c9cd21abbc273fd8741f4dbad
Brian McClune <bpmcclune@gmail.com>
2021-03-24 09:23:38 -0400

    Specify cryptography<3.4, no DeprecationWarning

--- server/setup.py
+++ server/setup.py
@@ -24,6 +24,7 @@ setuptools.setup(
         'fastapi',
         'uvicorn[standard]',
         'python-jose[cryptography]',
+        'cryptography<3.4',
         'httpx',
         'pydantic',
         'msal',
