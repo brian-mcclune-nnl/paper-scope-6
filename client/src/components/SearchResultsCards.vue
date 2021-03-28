2cf130ca3d9624c3d1cb8d5bfbde343875304c02
Brian McClune <bpmcclune@gmail.com>
2021-03-28 14:38:30 -0400

    Make UTC section of date string optional in format

--- client/src/components/SearchResultsCards.vue
+++ client/src/components/SearchResultsCards.vue
@@ -55,7 +55,7 @@
 
   const format = data => {
     const re =
-      /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)/
+      /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z)?/
     if (re.test(data))
       return new Intl.DateTimeFormat(
         'en-US',
