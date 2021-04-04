f08521186e716a5cbe6bebacf592e31bf6f04e45
Brian McClune <bpmcclune@gmail.com>
2021-04-04 09:14:32 -0400

    Add dev deps and scripts hook for tests, stub test

---
+++ client/test/App.test.js
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils'
+import App from '../src/App.vue'
+
+describe('App.test.js', () => {
+  test('renders a nav bar', () => {
+    expect('nav-bar').toBe('nav-bar')
+  })
+})
