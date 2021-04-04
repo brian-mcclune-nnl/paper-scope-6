c492857d64eb706ea806e706f7eecf32c242a5d1
Brian McClune <bpmcclune@gmail.com>
2021-04-04 17:16:22 -0400

    Update first test to include store, router plugins

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -1,8 +1,37 @@
 import { mount } from '@vue/test-utils'
+import { createStore } from 'vuex'
+import { createRouter, createWebHistory } from 'vue-router'
 import App from '../src/App.vue'
 
+
+const msal = {
+  namespaced: true,
+  actions: {
+    createInstance() {}
+  }
+}
+
+const store = createStore({
+  modules: { msal }
+})
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: '/', component: { template: 'Youkoso boku no app e' }}],
+})
+
 describe('App.test.js', () => {
-  test('renders a nav bar', () => {
+  beforeEach(async () => {
+    router.push('/')
+    await router.isReady()
+  })
+  test('renders a nav bar', async () => {
+    const wrapper = mount(App, {
+      global: { plugins: [store, router] },
+      shallow: true
+    })
+
+    console.log(wrapper.html())
     expect('nav-bar').toBe('nav-bar')
   })
 })
