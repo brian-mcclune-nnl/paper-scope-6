8d7e5c439e8a5c1a7f3bcbccb508d5fa04b90eb9
Brian McClune <bpmcclune@gmail.com>
2021-04-20 07:18:15 -0400

    Add window.matchMedia mock to App.test.js

--- client/test/App.test.js
+++ client/test/App.test.js
@@ -28,6 +28,21 @@ document.body.innerHTML = `
   </div>
 `
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+
 describe('App.test.js', () => {
   beforeEach(async () => {
     router.push('/')
